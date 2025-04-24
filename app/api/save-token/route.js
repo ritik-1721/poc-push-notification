3d5a92e8d486af896ad3dafe3fe844686ba9abe5
Ritik Pawar <ritikrppawar21@gmail.com>
2025-04-24 10:52:10 +0530

    a

--- app/api/save-token/route.js
+++ app/api/save-token/route.js
@@ -6,10 +6,29 @@ const app = getApps().length ? getApp() : initializeApp({ credential: cert(servi
 const db = getFirestore(app);
 
 export async function POST(request) {
-  console.log("serviceAccount:", serviceAccount);
-  console.log("Firebase app initialized:", app.name);
   const { token } = await request.json();
-  console.log("Received token:", token);    
-  await db.collection("fcmTokens").doc(token).set({ token });
-  return Response.json({ success: true });
+
+  const docRef = db.collection("fcmTokens").doc(token);
+  const docSnap = await docRef.get();
+
+  if (!docSnap.exists) {
+    await docRef.set({ token });
+    return Response.json({ success: true, message: "Token added." });
+  }
+
+  return Response.json({ success: true, message: "Token already exists." });
 }
+
+
+// import { cert, getApps, initializeApp, getApp } from "firebase-admin/app";
+// import { getFirestore } from "firebase-admin/firestore";
+
+// const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
+// const app = getApps().length ? getApp() : initializeApp({ credential: cert(serviceAccount) });
+// const db = getFirestore(app);
+
+// export async function POST(request) {
+//   const { token } = await request.json();
+//   await db.collection("fcmTokens").doc(token).set({ token });
+//   return Response.json({ success: true });
+// }
